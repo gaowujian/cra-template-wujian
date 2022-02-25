b2f103cb3a74e0b926004c2560ed0aae16e92aab
tony <gwjlshebei@gmail.com>
2022-02-25 18:25:30 +0800

    清理文件

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,17 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Button, message } from "antd";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <h1>Welcome</h1>
+      <Button
+        onClick={() => {
+          message.success("欢迎来到tony的cra模板");
+        }}
+      >
+        点击
+      </Button>
     </div>
   );
 }
